728d0e7eaa570b953739c915edef2054c4555872
Tayamul <rai_tayamul@hotmail.com>
2023-08-06 13:44:13 +0100

    Design a standard navbar layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import NavBar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <NavBar />   
+      <Navbar />   
     </>
   );
 }
