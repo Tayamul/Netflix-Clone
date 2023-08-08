dac844bc1a43bc98b811109094596a1db8e0babb
Tayamul <rai_tayamul@hotmail.com>
2023-08-08 11:05:12 +0100

    Add the heart icon to allow users to save movies in their database

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
-const Movie = ({item}) => {
+const Movie = ({ item }) => {
+  const [like, setLike] = useState(false)
   return (
     <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
       <img
@@ -11,6 +13,14 @@ const Movie = ({item}) => {
       <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
         <p className="white-space-normal text-xs md:text-sm font-bold flex items-center justify-center h-full text-center">
           {item?.title}
+        </p>
+        <p>
+          {" "}
+          {like ? (
+            <FaHeart className="absolute top-4 left-4 text-gray-300" />
+          ) : (
+            <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
+          )}{" "}
         </p>
       </div>
     </div>
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import Movie from "./Movie";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 const Row = ({ title, fetchURL }) => {
   const [movies, setMovies] = useState([]);
@@ -14,12 +15,23 @@ const Row = ({ title, fetchURL }) => {
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
-        <div id={"slider"}>
+      <div className="relative flex items-center group">
+        <MdChevronLeft
+          className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
+        <div
+          id={"slider"}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
           {movies.map((item, id) => (
-              <Movie key={id} item={item} />
+            <Movie key={id} item={item} />
           ))}
         </div>
+        <MdChevronRight
+          className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
       </div>
     </>
   );
