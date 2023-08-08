d32ca64d30b79bf56a36a87ccb9986e21936f594
Tayamul <rai_tayamul@hotmail.com>
2023-08-08 13:48:48 +0100

    Enable email-based sign-up feature

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,8 +12,6 @@ const Main = () => {
     });
   }, []);
 
-  console.log(movie);
-
   const truncateString = (str, num) => {
     return str?.length > num ? str.slice(0, num) + "..." : str;
   };
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,15 +1,45 @@
 import { createContext, useContext, useState, useEffect } from "react";
+import { auth } from "../firebase";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 
 const AuthContext = createContext();
 
-export const AuthContextProvider = (chilren) => {
-    return (
-        <AuthContext.Provider>
-            {chilren}
-        </AuthContext.Provider>
-    )
-}
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  const signUp = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const logIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ signUp, logIn, logOut, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export const UserAuth = () => {
-    return useContext(AuthContext);
-}
+  return useContext(AuthContext);
+};
