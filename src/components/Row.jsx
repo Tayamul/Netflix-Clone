e0cdc5981cac48daa49a80c56012ebd28ed7fb12
Tayamul <rai_tayamul@hotmail.com>
2023-08-08 11:18:42 +0100

    Add scrolling effect to each unique individul rows

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import Movie from "./Movie";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowId }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -11,17 +11,28 @@ const Row = ({ title, fetchURL }) => {
       setMovies(res.data.results);
     });
   }, [fetchURL]);
-  console.log(movies, "FETCH");
+
+  const slideLeft = () => {
+    var slider = document.getElementById('slider' + rowId)
+    slider.scrollLeft = slider.scrollLeft - 500
+  }
+
+  const slideRight = () => {
+    var slider = document.getElementById('slider' + rowId)
+    slider.scrollLeft = slider.scrollLeft + 500
+  }
+
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="relative flex items-center group">
         <MdChevronLeft
+          onClick={slideLeft}
           className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
           size={40}
         />
         <div
-          id={"slider"}
+          id={"slider" + rowId}
           className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
         >
           {movies.map((item, id) => (
@@ -29,6 +40,7 @@ const Row = ({ title, fetchURL }) => {
           ))}
         </div>
         <MdChevronRight
+          onClick={slideRight}
           className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
           size={40}
         />
