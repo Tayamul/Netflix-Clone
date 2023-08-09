b8b8b7351a7a34a145619658cfe92c525fe86b12
Tayamul <rai_tayamul@hotmail.com>
2023-08-09 23:47:50 +0100

    Enable users to delete movies off their saved list

--- src/components/SavedShows.jsx
+++ src/components/SavedShows.jsx
@@ -3,6 +3,7 @@ import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { UserAuth } from "../context/AuthContext";
 import { db } from "../firebase";
 import { updateDoc, doc, onSnapshot } from "firebase/firestore";
+import { AiOutlineClose } from "react-icons/ai";
 
 const SavedShows = () => {
   const [movies, setMovies] = useState([]);
@@ -19,10 +20,22 @@ const SavedShows = () => {
   };
 
   useEffect(() => {
-    onSnapshot(doc(db, 'users', `${user?.email}`), (doc) => {
-      setMovies(doc.data()?.savedShows)
-    })
-  }, [user?.email])
+    onSnapshot(doc(db, "users", `${user?.email}`), (doc) => {
+      setMovies(doc.data()?.savedShows);
+    });
+  }, [user?.email]);
+
+  const movieRef = doc(db, "users", `${user?.email}`);
+  const deleteShow = async (passedId) => {
+    try {
+      const result = movies.filter((item) => item.id !== passedId);
+      await updateDoc(movieRef, {
+        savedShows: result,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <>
@@ -37,8 +50,11 @@ const SavedShows = () => {
           id={"slider"}
           className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
         >
-          {movies.map((item, id) => (
-            <div key={id} className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
+          {movies.map((item) => (
+            <div
+              key={item.id}
+              className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2"
+            >
               <img
                 className="w-full h-auto block"
                 src={`https://image.tmdb.org/t/p/w500/${item?.img}`}
@@ -47,6 +63,12 @@ const SavedShows = () => {
               <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
                 <p className="white-space-normal text-xs md:text-sm font-bold flex items-center justify-center h-full text-center">
                   {item?.title}
+                </p>
+                <p
+                  onClick={() => deleteShow(item.id)}
+                  className="absolute text-gray-300 top-4 right-4"
+                >
+                  <AiOutlineClose />
                 </p>
               </div>
             </div>
